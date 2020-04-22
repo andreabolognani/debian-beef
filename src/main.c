c67f6309df0446a57a1a6d00ad15b796354cd900
Andrea Bolognani <eof@kiyuko.org>
2020-04-22 22:55:03 +0200

    New upstream version 1.2.0

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 /* Beef - Flexible Brainfuck interpreter
- * Copyright (C) 2005-2017  Andrea Bolognani <eof@kiyuko.org>
+ * Copyright (C) 2005-2020  Andrea Bolognani <eof@kiyuko.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -12,10 +12,9 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
- * Homepage: http://kiyuko.org/software/beef
+ * Homepage: https://kiyuko.org/software/beef
  */
 
 #include <glib.h>
@@ -28,6 +27,7 @@
 #include <readline/readline.h>
 #include <config.h>
 #include <unistd.h>
+#include "compat.h"
 #include "io.h"
 #include "options.h"
 
@@ -43,17 +43,17 @@ void
 display_error (const gchar *context,
                const gchar *message)
 {
-	if (context != NULL)
-	{
-		g_printerr ("%s: %s: %s\n", g_get_prgname (),
-		                            context,
-		                            message);
-	}
-	else
-	{
-		g_printerr ("%s: %s\n", g_get_prgname (),
-		                        message);
-	}
+    if (context != NULL)
+    {
+        g_printerr ("%s: %s: %s\n", g_get_prgname (),
+                    context,
+                    message);
+    }
+    else
+    {
+        g_printerr ("%s: %s\n", g_get_prgname (),
+                    message);
+    }
 }
 
 /**
@@ -65,278 +65,225 @@ gint
 main (gint    argc,
       gchar **argv)
 {
-	CattleInterpreter *interpreter;
-	CattleProgram     *program;
-	CattleBuffer      *buffer;
-	GFile             *file;
-	GFileOutputStream *output_stream;
-	GFileInputStream  *input_stream;
-	GOptionContext    *context;
-	GOptionGroup      *group;
-	GError            *error;
-	OptionValues      *option_values;
-	gboolean           success;
+    g_autoptr (CattleInterpreter)  interpreter = NULL;
+    g_autoptr (CattleProgram)      program = NULL;
+    g_autoptr (CattleBuffer)       buffer = NULL;
+    g_autoptr (GFileOutputStream)  output_stream = NULL;
+    g_autoptr (GFileInputStream)   input_stream = NULL;
+    g_autoptr (GError)             error = NULL;
+    g_autoptr (OptionValues)       option_values = NULL;
+    g_autoptr (GOptionContext)     context = NULL;
+    GOptionGroup                  *group;
+    gboolean                       success;
 
-#if !GLIB_CHECK_VERSION(2, 36, 0)
-	g_type_init ();
-#endif
+    g_set_prgname ("beef");
 
-	g_set_prgname ("beef");
+    /* Set up a configuration group for commanline options */
+    option_values = option_values_new ();
+    context = g_option_context_new ("- Flexible Brainfuck interpreter");
+    group = g_option_group_new ("",
+                                "",
+                                "",
+                                option_values,
+                                NULL);
+    g_option_group_add_entries (group,
+                                get_option_entries ());
+    g_option_context_set_main_group (context, group);
 
-	/* Set up a configuration group for commanline options */
-	option_values = g_new0 (OptionValues, 1);
-	option_values->configuration = cattle_configuration_new ();
-	context = g_option_context_new ("FILE - Flexible Brainfuck interpreter");
-	group = g_option_group_new ("",
-	                            "",
-	                            "",
-	                            option_values,
-	                            NULL);
-	g_option_group_add_entries (group,
-	                            get_option_entries ());
-	g_option_context_set_main_group (context, group);
+    /* Parse commandline options */
+    error = NULL;
+    success = g_option_context_parse (context, &argc, &argv, &error);
 
-	/* Parse commandline options */
-	error = NULL;
-	success = g_option_context_parse (context, &argc, &argv, &error);
+    if (!success)
+    {
+        display_error (NULL,
+                       error->message);
 
-	if (!success)
-	{
-		display_error (NULL,
-		               error->message);
+        return 1;
+    }
 
-		g_object_unref (option_values->configuration);
-		g_free (option_values->output_filename);
-		g_free (option_values->input_filename);
-		g_free (option_values);
+    /* The program will be provided either as a file name (in which case
+     * we need to load it from there) or directly on the command line.
+     * The option parser makes sure we have either one or the other */
+    if (option_values->program_filename)
+    {
+        g_autoptr (GFile) file = NULL;
 
-		return 1;
-	}
+        /* Load file contents */
+        file = g_file_new_for_commandline_arg (option_values->program_filename);
 
-	/* Make sure a file has been specified on the commandline */
-	if (argc != 2)
-	{
-		g_printerr ("Usage: %s [OPTION...] FILE\n", g_get_prgname ());
+        error = NULL;
+        buffer = load_file_contents (file,
+                                     &error);
 
-		g_object_unref (option_values->configuration);
-		g_free (option_values->output_filename);
-		g_free (option_values->input_filename);
-		g_free (option_values);
+        if (buffer == NULL)
+        {
+            display_error (option_values->program_filename,
+                           error->message);
 
-		return 1;
-	}
+            return 1;
+        }
+    }
+    else if (option_values->program)
+    {
+        gulong size;
 
-	/* Load file contents */
-	file = g_file_new_for_commandline_arg (argv[1]);
+        size = strlen (option_values->program);
 
-	error = NULL;
-	buffer = load_file_contents (file,
-	                             &error);
-	g_object_unref (file);
+        buffer = cattle_buffer_new (size);
+        cattle_buffer_set_contents(buffer, (gint8 *) option_values->program);
+    }
+    else
+    {
+        g_printerr ("Usage: %s [OPTION?] FILE\n", g_get_prgname ());
 
-	if (buffer == NULL)
-	{
-		display_error (argv[1],
-		               error->message);
+        return 1;
+    }
 
-		g_object_unref (option_values->configuration);
-		g_free (option_values->output_filename);
-		g_free (option_values->input_filename);
-		g_free (option_values);
+    interpreter = cattle_interpreter_new ();
+    program = cattle_interpreter_get_program (interpreter);
 
-		return 1;
-	}
+    /* Load program */
+    error = NULL;
+    success = cattle_program_load (program,
+                                   buffer,
+                                   &error);
 
-	interpreter = cattle_interpreter_new ();
-	program = cattle_interpreter_get_program (interpreter);
+    if (!success)
+    {
+        display_error (option_values->program_filename,
+                       error->message);
 
-	/* Load program */
-	error = NULL;
-	success = cattle_program_load (program,
-	                               buffer,
-	                               &error);
-	g_object_unref (program);
-	g_object_unref (buffer);
+        return 1;
+    }
 
-	if (!success)
-	{
-		display_error (argv[1],
-		               error->message);
+    /* Assign configuration created using commandline options to the
+     * interpreter */
+    cattle_interpreter_set_configuration (interpreter,
+                                          option_values->configuration);
 
-		g_object_unref (interpreter);
-		g_object_unref (option_values->configuration);
-		g_free (option_values->output_filename);
-		g_free (option_values->input_filename);
-		g_free (option_values);
+    /* If output to file was chosen, open the selected output file and
+     * assign a suitable output handler to the interpreter */
+    if (option_values->output_filename != NULL)
+    {
+        g_autoptr (GFile) file = NULL;
 
-		return 1;
-	}
+        file = g_file_new_for_commandline_arg (option_values->output_filename);
 
-	output_stream = NULL;
-	input_stream = NULL;
+        error = NULL;
+        output_stream = g_file_replace (file,
+                                        NULL,  /* No etag */
+                                        FALSE, /* No backups */
+                                        G_FILE_CREATE_NONE,
+                                        NULL,
+                                        &error);
 
-	/* Assign configuration created using commandline options to the
-	 * interpreter */
-	cattle_interpreter_set_configuration (interpreter,
-	                                      option_values->configuration);
-	g_object_unref (option_values->configuration);
+        if (error != NULL)
+        {
+            display_error (option_values->output_filename,
+                           error->message);
 
-	/* If output to file was chosen, open the selected output file and
-	 * assign a suitable output handler to the interpreter */
-	if (option_values->output_filename != NULL)
-	{
-		file = g_file_new_for_commandline_arg (option_values->output_filename);
+            return 1;
+        }
+    }
 
-		error = NULL;
-		output_stream = g_file_replace (file,
-		                                NULL,  /* No etag */
-		                                FALSE, /* No backups */
-		                                G_FILE_CREATE_NONE,
-		                                NULL,
-		                                &error);
-		g_object_unref (file);
+    /* Set output handler for the interpreter */
+    cattle_interpreter_set_output_handler (interpreter,
+                                           output_handler,
+                                           output_stream);
 
-		if (error != NULL)
-		{
-			display_error (option_values->output_filename,
-			               error->message);
+    /* If input from file was chosen, open the selected input file and
+     * assign a suitable input handler to the interpreter */
+    if (option_values->input_filename != NULL)
+    {
+        g_autoptr (GFile) file = NULL;
 
-			g_object_unref (interpreter);
-			g_free (option_values->output_filename);
-			g_free (option_values->input_filename);
-			g_free (option_values);
+        file = g_file_new_for_commandline_arg (option_values->input_filename);
 
-			return 1;
-		}
-	}
+        error = NULL;
+        input_stream = g_file_read (file,
+                                    NULL,
+                                    &error);
 
-	/* Set output handler for the interpreter */
-	cattle_interpreter_set_output_handler (interpreter,
-	                                       output_handler,
-	                                       output_stream);
+        if (error != NULL)
+        {
+            display_error (option_values->input_filename,
+                           error->message);
 
-	/* If input from file was chosen, open the selected input file and
-	 * assign a suitable input handler to the interpreter */
-	if (option_values->input_filename != NULL)
-	{
-		file = g_file_new_for_commandline_arg (option_values->input_filename);
+            return 1;
+        }
 
-		error = NULL;
-		input_stream = g_file_read (file,
-		                            NULL,
-		                            &error);
-		g_object_unref (file);
+        /* Set input handler for the interpreter */
+        cattle_interpreter_set_input_handler (interpreter,
+                                              input_handler,
+                                              input_stream);
+    }
+    else
+    {
+        /* Use readline only if standard input is connected to a
+         * terminal; otherwise, eg. when using shell input redirection,
+         * the input is displayed along with the output, which is not nice */
+        if (isatty (0))
+        {
+            /* Initialize readline */
+            rl_initialize ();
 
-		if (error != NULL)
-		{
-			display_error (option_values->input_filename,
-			               error->message);
+            /* Disable filename completion on TAB */
+            rl_bind_key ('\t', rl_insert);
 
-			g_object_unref (output_stream);
-			g_object_unref (interpreter);
-			g_free (option_values->output_filename);
-			g_free (option_values->input_filename);
-			g_free (option_values);
+            /* Use a readline-based interactive input handler */
+            cattle_interpreter_set_input_handler (interpreter,
+                                                  input_handler_interactive,
+                                                  NULL);
+        }
+    }
 
-			return 1;
-		}
+    /* Run program */
+    error = NULL;
+    success = cattle_interpreter_run (interpreter,
+                                      &error);
 
-		/* Set input handler for the interpreter */
-		cattle_interpreter_set_input_handler (interpreter,
-		                                      input_handler,
-		                                      input_stream);
-	}
-	else
-	{
-		/* Use readline only if standard input is connected to a
-		 * terminal; otherwise, eg. when using shell input redirection,
-		 * the input is displayed along with the output, which is not nice */
-		if (isatty (0))
-		{
-			/* Initialize readline */
-			rl_initialize ();
+    if (!success)
+    {
+        display_error (option_values->program_filename,
+                       error->message);
 
-			/* Disable filename completion on TAB */
-			rl_bind_key ('\t', rl_insert);
+        return 1;
+    }
 
-			/* Use a readline-based interactive input handler */
-			cattle_interpreter_set_input_handler (interpreter,
-			                                      input_handler_interactive,
-			                                      NULL);
-		}
-	}
+    if (output_stream != NULL)
+    {
+        /* Close the output file */
+        error = NULL;
+        g_output_stream_close (G_OUTPUT_STREAM (output_stream),
+                               NULL,
+                               &error);
 
-	/* Run program */
-	error = NULL;
-	success = cattle_interpreter_run (interpreter,
-	                                  &error);
+        if (error != NULL)
+        {
+            display_error (option_values->output_filename,
+                           error->message);
 
-	if (!success)
-	{
-		display_error (argv[1],
-		               error->message);
+            return 1;
+        }
+    }
 
-		g_object_unref (input_stream);
-		g_object_unref (output_stream);
-		g_object_unref (interpreter);
-		g_free (option_values->output_filename);
-		g_free (option_values->input_filename);
-		g_free (option_values);
+    if (input_stream != NULL)
+    {
+        /* Close the input file */
+        error = NULL;
+        g_input_stream_close (G_INPUT_STREAM (input_stream),
+                              NULL,
+                              &error);
 
-		return 1;
-	}
+        if (error != NULL) {
 
-	if (output_stream != NULL)
-	{
-		/* Close the output file */
-		error = NULL;
-		g_output_stream_close (G_OUTPUT_STREAM (output_stream),
-		                       NULL,
-		                       &error);
-		g_object_unref (output_stream);
+            display_error (option_values->input_filename,
+                           error->message);
 
-		if (error != NULL)
-		{
-			display_error (option_values->output_filename,
-			               error->message);
+            return 1;
+        }
+    }
 
-			g_object_unref (input_stream);
-			g_object_unref (interpreter);
-			g_free (option_values->output_filename);
-			g_free (option_values->input_filename);
-			g_free (option_values);
-
-			return 1;
-		}
-	}
-
-	if (input_stream != NULL)
-	{
-		/* Close the input file */
-		error = NULL;
-		g_input_stream_close (G_INPUT_STREAM (input_stream),
-		                      NULL,
-		                      &error);
-		g_object_unref (input_stream);
-
-		if (error != NULL) {
-
-			display_error (option_values->input_filename,
-			               error->message);
-
-			g_object_unref (interpreter);
-			g_free (option_values->output_filename);
-			g_free (option_values->input_filename);
-			g_free (option_values);
-
-			return 1;
-		}
-	}
-
-	g_object_unref (interpreter);
-	g_free (option_values->output_filename);
-	g_free (option_values->input_filename);
-	g_free (option_values);
-
-	return 0;
+    return 0;
 }
